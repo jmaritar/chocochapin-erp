6fbec2b6157b3c704f8a3ed6c856bcc58811183a
Mario Arita <marioarita502@gmail.com>
2023-07-23 18:48:53 -0600

    feat(config): eslint configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,40 @@
+'use client'
+
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+
   return (
-    <main className="flex flex-col items-center justify-between p-24 bg-gray-100">
-      <h1 className="text-5xl font-bold mb-5 text-blue-600">Bienvenido al proyecto ChocoChapin ERP</h1>
+    <main className="flex flex-col items-center justify-between bg-gray-100 p-24">
+
+      <h1 className="mb-5 text-5xl font-bold text-blue-600">Bienvenido al proyecto ChocoChapin ERP</h1>
       <p className="mb-5 text-lg">Este proyecto consta de varios módulos, a continuación se presenta una descripción de cada uno y los encargados de su desarrollo:</p>
 
-      <div className="flex flex-wrap justify-center mb-10">
-        <div className="m-4 bg-blue-100 shadow-md rounded-lg p-6 w-64">
-          <h2 className="text-xl font-semibold mb-2 text-blue-600">Módulo 1: Gestión de inventario</h2>
+      <div className="mb-10 flex flex-wrap justify-center">
+        <div className="m-4 w-64 rounded-lg bg-blue-100 p-6 shadow-md" onClick={() => { router.push('/borrar1') }}>
+          <h2 className="mb-2 text-xl font-semibold text-blue-600">Módulo 1: Gestión de inventario</h2>
           <p>Encargado: Mario Arita - <code>@dev_ja</code></p>
         </div>
 
-        <div className="m-4 bg-green-100 shadow-md rounded-lg p-6 w-64">
-          <h2 className="text-xl font-semibold mb-2 text-green-600">Módulo 2: Gestión de ventas</h2>
+        <div className="m-4 w-64 rounded-lg bg-green-100 p-6 shadow-md">
+          <h2 className="mb-2 text-xl font-semibold text-green-600">Módulo 2: Gestión de ventas</h2>
           <p>Encargado: Nery Lopez - <code>@dev_nl</code></p>
         </div>
 
-        <div className="m-4 bg-yellow-100 shadow-md rounded-lg p-6 w-64">
-          <h2 className="text-xl font-semibold mb-2 text-yellow-600">Módulo 3: Gestión de clientes</h2>
+        <div className="m-4 w-64 rounded-lg bg-yellow-100 p-6 shadow-md">
+          <h2 className="mb-2 text-xl font-semibold text-yellow-600">Módulo 3: Gestión de clientes</h2>
           <p>Encargado: Heidy Chu - <code>@dev_hc</code></p>
         </div>
 
-        <div className="m-4 bg-red-100 shadow-md rounded-lg p-6 w-64">
-          <h2 className="text-xl font-semibold mb-2 text-red-600">Módulo 4: Gestión financiera</h2>
+        <div className="m-4 w-64 rounded-lg bg-red-100 p-6 shadow-md">
+          <h2 className="mb-2 text-xl font-semibold text-red-600">Módulo 4: Gestión financiera</h2>
           <p>Encargado: Miguel Alvarez - <code>@dev_ma</code></p>
         </div>
 
-        <div className="m-4 bg-purple-100 shadow-md rounded-lg p-6 w-64">
-          <h2 className="text-xl font-semibold mb-2 text-purple-600">Módulo 5: Login y Seguridad</h2>
+        <div className="m-4 w-64 rounded-lg bg-purple-100 p-6 shadow-md">
+          <h2 className="mb-2 text-xl font-semibold text-purple-600">Módulo 5: Login y Seguridad</h2>
           <p>Encargado: Marvin Martinez - <code>@dev_mm</code></p>
         </div>
       </div>
--- src/app/themeApp/page.tsx
+++ src/app/themeApp/page.tsx
@@ -1,47 +1,47 @@
 export default function Colors() {
   const colors = [
     {
-      name: "Fondo principal",
-      hex: "#F2F2F2",
+      name: 'Fondo principal',
+      hex: '#F2F2F2'
     },
     {
-      name: "Acento azul",
-      hex: "#89CFF0",
+      name: 'Acento azul',
+      hex: '#89CFF0'
     },
     {
-      name: "Acento rosa",
-      hex: "#FFB6C1",
+      name: 'Acento rosa',
+      hex: '#FFB6C1'
     },
     {
-      name: "Acento amarillo",
-      hex: "#FFFACD",
+      name: 'Acento amarillo',
+      hex: '#FFFACD'
     },
     {
-      name: "Acento verde",
-      hex: "#98FB98",
+      name: 'Acento verde',
+      hex: '#98FB98'
     },
     {
-      name: "Acento morado",
-      hex: "#E6E6FA",
-    },
-  ];
+      name: 'Acento morado',
+      hex: '#E6E6FA'
+    }
+  ]
 
   return (
-  
+
     <div className="p-10">
-      <h1 className="text-3xl font-bold mb-5">Paleta de Colores</h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <h1 className="mb-5 text-3xl font-bold">Paleta de Colores</h1>
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {colors.map((color) => (
           <div
             key={color.name}
-            className="border rounded-lg p-4 text-center"
+            className="rounded-lg border p-4 text-center"
             style={{ backgroundColor: color.hex }}
           >
-            <div className="text-lg font-semibold mb-2">{color.name}</div>
-            <div className="text-sm font-mono">{color.hex}</div>
+            <div className="mb-2 text-lg font-semibold">{color.name}</div>
+            <div className="font-mono text-sm">{color.hex}</div>
           </div>
         ))}
       </div>
     </div>
-  );
+  )
 };
